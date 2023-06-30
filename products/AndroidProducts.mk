d2188b8dd12d513938f780f12a02a1d670078db3
BladeRunner-A2C <john.smith@unused.email>
2023-06-30 15:57:17 +0600

    products: Add Xiaomi POCO X5 Pro (redwood)
    Change-Id: I04cdc0c23db55054878d546566b5a8412560a15e
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -51,6 +51,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/psyche/aospa_psyche.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
+    $(LOCAL_DIR)/redwood/aospa_redwood.mk \
     $(LOCAL_DIR)/sky/aospa_sky.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
     $(LOCAL_DIR)/sunny/aospa_sunny.mk \
@@ -94,6 +95,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_porsche-userdebug \
     aospa_psyche-userdebug \
     aospa_raven-userdebug \
+    aospa_redwood-userdebug \
     aospa_sky-userdebug \
     aospa_spes-userdebug \
     aospa_sunny-userdebug \
